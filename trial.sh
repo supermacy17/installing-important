512bfc13ed73cbc97db861ca9792c51a6c0d3bfc
supermacy17 <rahulanandsinha@gmail.com>
2015-11-27 04:54:53 +0530

    ubproxy error fixed vim added

--- trial.sh
+++ trial.sh
@@ -1,26 +1,65 @@
+#!/bin/bash
+#usage ss1
+#bin variables
+$proxy_server
+$port
+$choice
+echo "Enter your proxy server"
+read proxy_server
+
+echo "Enter port "
+read port
+echo "Use Authentication[y/n]"
+read choice
+
+proxy_server+=":"
+proxy_server=$proxy_server$port
+
+export http_proxy='http://'$proxy_server
+export https_proxy='https://'$proxy_server 
+export ftp_proxy='ftp://'$proxy_server
+
 dpkg --add-architecture i386
 
 
+
 #getting files for ubproxy
-wget  "www.code.google.com/p/ubproxy/"
+
+
+if ! curl "http://ubproxy.googlecode.com/files/ubproxy">ubproxy; then
+  echo "ERROR: cannot able download ubproxy" >&2
+  exit 1
+fi
+chmod u+x ubproxy
+./ubproxy
+
 #adding repositories
+#for skype
 add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 
 echo "updating the system and then it will install the package" 
-#apt-get update
+#apt-get -y update
+
+
+
 #installing all the software in the system
 echo "installing sensors"
-#apt-get install lm-sensors
+apt-get -y install lm-sensors>/dev/null
 echo "installing skype"
-sleep 1
-#apt-get install skype
+
+apt-get -y install skype >/dev/null
 echo "installing vlc"
-#apt-get install vlc
+apt-get -y install vlc>/dev/null
 
 echo "installing curl"
-apt-get install curl
+apt-get -y install curl>/dev/null
 
 echo "installing git"
-apt-get install git
+apt-get -y install git>/dev/null
+
+echo "installing vim"
+
+apt-get -y install vim>/dev/null
+
 
 
