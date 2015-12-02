dc573557d3cbcd9d54cc69e8d4b690c8952b2740
supermacy17 <rahulanandsinha@gmail.com>
2015-12-02 18:37:12 +0530

    trial message

--- trial.sh
+++ trial.sh
@@ -21,70 +21,26 @@ export ftp_proxy='ftp://'$proxy_server
 
 
 
-
-
-
-dpkg --add-architecture i386
-
-
+export http_proxy='http://ipg_2011084:931170@192.168.1.107:3128'
+export https_proxy='https://ipg_2011084:931170@192.168.1.107:3128'
+export ftp_proxy='ftp://ipg_2011084:931170@192.168.1.107:3128'
 
 #getting files for ubproxy
 
 
-if ! curl "http://ubproxy.googlecode.com/files/ubproxy">ubproxy; then
-  echo "ERROR: cannot able download ubproxy" >&2
-  exit 1
-fi
-chmod u+x ubproxy
-./ubproxy
 
-#adding repositories
-#for skype
-add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-echo "adding repo of java"
-add-apt-repository ppa:webupd8team/java
+echo "now downloading pycharm"
 
+	if ! wget -O pycharm_professional.tar.gz "http://download.jetbrains.com/python/pycharm-professional-4.5.4.tar.gz" ; then
+		echo "Error:Unable to download Pycharm professional version">&2
+		exit 1
+	fi
 
-echo "updating the system and then it will install the package" 
-apt-get -y --force-yes update
+	tar -zxvf pycharm_professional.tar.gz
 
+cd pycharm-4.5.4/bin
+./pycharm.sh
 
-
-#installing all the software in the system
-echo "installing sensors"
-apt-get -y --force-yes install lm-sensors>/dev/null
-echo "installing skype"
-
-apt-get -y --force-yes install skype >/dev/null
-echo "installing vlc"
-apt-get -y --force-yes install vlc>/dev/null
-
-echo "installing curl"
-apt-get -y install --force-yes curl>/dev/null
-
-echo "installing git"
-apt-get -y install--force-yes  git>/dev/null
-
-echo "installing vim"
-
-apt-get -y --force-yes install vim>/dev/null
-
-echo "installing default jre "
- 
-apt-get -y --force-yes install default-jre>/dev/null
-
-echo "installing default jdk"
-apt-get -y --force-yes install default-jdk>/dev/null
-
-echo "installing open jre"
-apt-get -y --force-yes install openjdk-7-jre>/dev/null 
-
-echo "installing open jdk"
-apt-get -y --force-yes install openjdk-7-jdk>/dev/null
-
-echo "installing oracle java installer"
-apt-get -y --force-yes install python-software-properties>/dev/null
-
-apt-get -y --force-yes install oracle-java7-installer>/dev/null
-
+echo "now installing unrar"
+apt-get -y --force yes install unrar
 
